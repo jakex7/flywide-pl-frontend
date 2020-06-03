58d4ece4f496dee1662f0cee568e7d2c4fd0869e
Piotr Czupyk <piotr.czupyk@gmail.com>
2020-06-03 19:14:01 +0200

    'test'

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,10 @@
-import React  from "react";
+import React from "react";
 import Login from "../Login/Login";
 import Logout from "../Logout/Logout";
 import { Link } from "react-router-dom";
 
 import { useSelector } from "react-redux";
-import {
-    selectIsLogged,
-    selectNickname
-} from "../../features/user/userSlice";
+import { selectIsLogged, selectNickname } from "../../features/user/userSlice";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -15,37 +12,38 @@ import Typography from "@material-ui/core/Typography";
 import styles from "./NavBar.module.scss";
 import StatusBar from "./StatusBar";
 
-const NavBar = ({clientsOnline, clientsMax}) => {
-    const isLogged = useSelector(selectIsLogged);
-    const nickname = useSelector(selectNickname);
+const NavBar = ({ clientsOnline, clientsMax }) => {
+  const isLogged = useSelector(selectIsLogged);
+  const nickname = useSelector(selectNickname);
 
-    return (
-        <>
-            <StatusBar nickname={nickname} isLogged={isLogged} clientsOnline={clientsOnline} clientsMax={clientsMax} />
-            <AppBar position="static" color="inherit" className={styles.navBar}>
-                <Toolbar className={styles.container}>
-                    <Typography variant="h6" noWrap style={{flexGrow: 1}}>
-                        <Link to="/" className={styles.link}>
-                            Flywide
-                        </Link>
-                    </Typography>
-                    <div>
-                        <Link to="/user" className={styles.link}>
-                            Client Info
-                        </Link>
-                        <Link to="/assigner" className={styles.link}>
-                            Assigner
-                        </Link>
-                        {
-                            isLogged
-                                ? <Logout/>
-                                : <Login/>
-                        }
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </>
-    )
+  return (
+    <>
+      <StatusBar
+        nickname={nickname}
+        isLogged={isLogged}
+        clientsOnline={clientsOnline}
+        clientsMax={clientsMax}
+      />
+      <AppBar position="static" color="inherit" className={styles.navBar}>
+        <Toolbar className={styles.container}>
+          <Typography variant="h6" noWrap style={{ flexGrow: 1 }}>
+            <Link to="/" className={styles.link}>
+              Flywide
+            </Link>
+          </Typography>
+          <div>
+            <Link to="/user" className={styles.link}>
+              Informacje klienta
+            </Link>
+            <Link to="/assigner" className={styles.link}>
+              Nadawanie rang
+            </Link>
+            {isLogged ? <Wyloguj /> : <Zaloguj />}
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default NavBar;
--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -1,20 +1,22 @@
 import React from "react";
 import styles from "./Status.module.scss";
 import Rank from "./Rank";
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
-const Status = ({adminsStatus}) => {
-    return (
-        <div className={styles.statusContainer}>
-            <h2 className={styles.statusHeader}>Admins online</h2>
-            <Card>
-                <CardContent>
-                    {adminsStatus.map(adminsStatusGroup => <Rank group={adminsStatusGroup} key={adminsStatusGroup.groupId}/>)}
-                </CardContent>
-            </Card>
-        </div>
-    );
+const Status = ({ adminsStatus }) => {
+  return (
+    <div className={styles.statusContainer}>
+      <h2 className={styles.statusHeader}>Dostępni Administratorzy</h2>
+      <Card>
+        <CardContent>
+          {adminsStatus.map((adminsStatusGroup) => (
+            <Rank group={adminsStatusGroup} key={adminsStatusGroup.groupId} />
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Status;
